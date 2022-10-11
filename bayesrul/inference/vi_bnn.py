bug corrections (e.g. nll computation) and other fixes

bayesrul/inference/vi_bnn.py
@@ -1,17 +1,17 @@
 # type: ignore
+import logging
 from pathlib import Path
-from tqdm import tqdm
-
-import torch
 
 import pytorch_lightning as pl
+import torch
+from pytorch_lightning.callbacks import EarlyStopping, ModelCheckpoint
+from tqdm import tqdm
 
 from bayesrul.inference.inference import Inference
-from bayesrul.lightning_wrappers.frequentist import DnnPretrainWrapper
 from bayesrul.lightning_wrappers.bayesian import VIBnnWrapper
-from bayesrul.utils.miscellaneous import get_checkpoint, Dotdict
+from bayesrul.lightning_wrappers.frequentist import DnnPretrainWrapper
+from bayesrul.utils.miscellaneous import Dotdict, get_checkpoint
 from bayesrul.utils.post_process import ResultSaver
-from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
 
 DEBUG = True
 
@@ -106,7 +106,7 @@ class VI_BNN(Inference):
 
         self.args.device = torch.device(f"cuda:{self.GPU}")
         if checkpoint_file:
-            print("Loading already trained model")
+            print(f"Loading already trained model from {checkpoint_file}")
             additional_kwargs = {"pretrain": 0, "pretrain_file": None}
             self.bnn = VIBnnWrapper.load_from_checkpoint(
                 checkpoint_file,
@@ -158,6 +158,7 @@ class VI_BNN(Inference):
             self._define_model()
 
         tester = pl.Trainer(
+            default_root_dir=self.base_log_dir,
             accelerator="gpu",
             devices=[self.GPU],
             log_every_n_steps=100,

bayesrul/lightning_wrappers/bayesian.py
@@ -291,24 +291,24 @@ class VIBnnWrapper(BnnWrapper):
 
     def test_step(self, batch, batch_idx):
         (x, y) = batch[0], batch[1].squeeze()
-        with self.fit_ctxt():
-            output = self.bnn.predict(x, num_predictions=10)
-            if isinstance(output, tuple):
-                loc, scale = output
-            else:
-                output = output.squeeze()
-                loc, scale = output[:, 0], output[:, 1]
+        output = self.bnn.predict(x, num_predictions=10, aggregate=True)
+        if isinstance(output, tuple):
+            loc, scale = output
+        else:
+            loc, scale = output[:, 0], output[:, 1]
 
+        nll = F.gaussian_nll_loss(loc, y, torch.square(scale))
         mse = F.mse_loss(y.squeeze(), loc)
         rmsce = rms_calibration_error(loc, scale, y.squeeze())
         sharp = sharpness(scale)
+        self.log("nll/test", nll)
         self.log("mse/test", mse)
         self.log("rmsce/test", rmsce)
         self.log("sharp/test", sharp)
 
         try:
             return {
-                "loss": mse,
+                "loss": nll,
                 "label": batch[1],
                 "pred": loc.squeeze(),
                 "std": scale,
@@ -325,17 +325,13 @@ class VIBnnWrapper(BnnWrapper):
             self.bnn_no_obs, self.bnn.guide, self.optimizer, self.loss
         )
 
-        with self.fit_ctxt():
-            elbo = self.svi.evaluate_loss(x, y.unsqueeze(-1))
-            # Aggregate = False if num_prediction = 1, else nans in sd
-            output = self.bnn.predict(x, num_predictions=1, aggregate=False)
+        elbo = self.svi.evaluate_loss(x, y.unsqueeze(-1))
+        # Aggregate = False if num_prediction = 1, else nans in sd
+        output = self.bnn.predict(x, num_predictions=1, aggregate=False)
+        output = output.squeeze()
+        loc, scale = output[:, 0], output[:, 1]
 
-            if isinstance(output, tuple):
-                loc, scale = output
-            else:
-                output = output.squeeze()
-                loc, scale = output[:, 0], output[:, 1]
-            kl = self.svi_no_obs.evaluate_loss(x)
+        kl = self.svi_no_obs.evaluate_loss(x)
 
         mse = F.mse_loss(y.squeeze(), loc)
         rmsce = rms_calibration_error(loc, scale, y.squeeze())
@@ -364,23 +360,20 @@ class VIBnnWrapper(BnnWrapper):
             elbo = self.svi.step(x, y.unsqueeze(-1))
             # Aggregate = False if num_prediction = 1, else nans in sd
             output = self.bnn.predict(x, num_predictions=1, aggregate=False)
-            if isinstance(output, tuple):  # Homoscedastic
-                loc, scale = output
-            else:  # Heteroscedastic
-                output = output.squeeze()
-                loc, scale = output[:, 0], output[:, 1]
+            output = output.squeeze()
+            loc, scale = output[:, 0], output[:, 1]
             kl = self.svi_no_obs.evaluate_loss(x)
         self.trainer.fit_loop.epoch_loop.batch_loop.manual_loop.optim_step_progress.increment_completed()
 
         mse = F.mse_loss(y.squeeze(), loc.squeeze()).item()
         rmsce = rms_calibration_error(loc, scale, y.squeeze())
         sharp = sharpness(scale)
-        self.log("mse/train", mse)
-        self.log("elbo/train", elbo)
-        self.log("kl/train", kl)
-        self.log("likelihood/train", elbo - kl)
-        self.log("rmsce/train", rmsce)
-        self.log("sharp/train", sharp)
+        self.log("mse/train", mse, on_step=False, on_epoch=True)
+        self.log("elbo/train", elbo, on_step=False, on_epoch=True)
+        self.log("kl/train", kl, on_step=False, on_epoch=True)
+        self.log("likelihood/train", elbo - kl, on_step=False, on_epoch=True)
+        self.log("rmsce/train", rmsce, on_step=False, on_epoch=True)
+        self.log("sharp/train", sharp, on_step=False, on_epoch=True)
 
 
 class MCMCBnnWrapper(BnnWrapper):

bayesrul/lightning_wrappers/frequentist.py
@@ -4,7 +4,6 @@ import torch
 import torch.nn as nn
 from torch.functional import F
 
-import numpy as np
 from bayesrul.models.conv import Conv
 from bayesrul.models.inception import BigCeption, InceptionModel
 from bayesrul.models.linear import Linear
@@ -127,7 +126,7 @@ class DnnWrapper(pl.LightningModule):
         else:
             loss = self.criterion(y_hat, y)
 
-        self.log(f"{self.loss}/{phase}", loss)
+        self.log(f"{self.loss}/{phase}", loss, on_step=False, on_epoch=True)
         if return_pred:
             if output.shape[1] == 2:
                 return loss, y_hat, scale
@@ -144,9 +143,9 @@ class DnnWrapper(pl.LightningModule):
             mse = F.mse_loss(loc, batch[1])
             rmsce = rms_calibration_error(loc, scale, batch[1])
             sharp = sharpness(scale)
-            self.log("mse/train", mse)
-            self.log("rmsce/train", rmsce)
-            self.log("sharp/train", sharp)
+            self.log("mse/train", mse, on_step=False, on_epoch=True)
+            self.log("rmsce/train", rmsce, on_step=False, on_epoch=True)
+            self.log("sharp/train", sharp, on_step=False, on_epoch=True)
         else:
             loss = self._compute_loss(batch, "train", return_pred=False)
         return loss
@@ -217,7 +216,7 @@ class DnnWrapper(pl.LightningModule):
             preds = []
             losses = []
             scales = []
-            for i in range(100):
+            for i in range(10):
                 if self.out_size == 2:
                     loss, loc, scale = self._compute_loss(
                         batch, "test", return_pred=True
@@ -234,7 +233,10 @@ class DnnWrapper(pl.LightningModule):
             preds = torch.stack(preds)
             if self.out_size == 2:
                 scales = torch.stack(scales)
-                scale = scales.mean(axis=0)
+                # scale = scales.mean(axis=0) NO!!!!
+                scale = (
+                    scales.pow(2).mean(0).add(preds.var(0)).sqrt()
+                )  # Like in TyXe
             else:
                 scale = preds.std(axis=0)
             preds = preds.mean(axis=0)
@@ -266,7 +268,6 @@ class DnnWrapper(pl.LightningModule):
                 labels = torch.cat([labels, output["label"].detach()])
                 if (self.dropout > 0) | (self.loss == "gaussian_nll"):
                     stds = torch.cat([stds, output["std"].detach()])
-
         self.test_preds["preds"] = preds.cpu().numpy()
         self.test_preds["labels"] = labels.cpu().numpy()
 
@@ -275,9 +276,10 @@ class DnnWrapper(pl.LightningModule):
 
         if (self.dropout > 0) | (self.loss == "gaussian_nll"):
             self.test_preds["stds"] = stds.cpu().numpy()
-
+            nll = F.gaussian_nll_loss(preds, labels, torch.square(stds))
             rmsce = rms_calibration_error(preds, stds, labels)
             sharp = sharpness(stds)
+            self.log("nll/test", nll)
             self.log("rmsce/test", rmsce)
             self.log("sharp/test", sharp)
 

bayesrul/ncmapss/metrics_tabular.py
@@ -116,7 +116,11 @@ def get_all_metrics(names: List[str]) -> pd.DataFrame:
 
             sharp = sharpness(std).cpu().item()
             rmsce = rms_calibration_error(y_pred, std, y_true).cpu().item()
-            nll = gaussian_nll_loss(y_pred, y_true, std).cpu().item()
+            nll = (
+                gaussian_nll_loss(y_pred, y_true, torch.square(std))
+                .cpu()
+                .item()
+            )
 
             # nasa = nasa_scoring_function(y_true, y_pred)
             rmse = torch.sqrt(((y_true - y_pred) ** 2).mean()).cpu().item()

bayesrul/ncmapss/train_best_models.py
@@ -66,15 +66,15 @@ if __name__ == "__main__":
         type=int,
         default=0,
         metavar="EARLY_STOP",
-        required=True,
+        required=False,
         help="Early stopping patience (default:0 early stooping disabled",
     )
     parser.add_argument(
         "--epochs",
         type=int,
-        default=None,
+        default=1,
         metavar="EPOCHS",
-        required=True,
+        required=False,
         help="Number of epochs",
     )
     parser.add_argument(
@@ -90,8 +90,14 @@ if __name__ == "__main__":
         type=int,
         default=1,
         metavar="NUM_RUNS",
-        required=True,
+        required=False,
         help="Number of runs (default: 1)",
+    )
+    parser.add_argument(
+        "--test",
+        action="store_true",
+        default=False,
+        help="Just run the test phase (default: False)",
     )
 
     args = parser.parse_args()
@@ -119,7 +125,7 @@ if __name__ == "__main__":
             print(f"--------------- {args.model_name} -------------------")
 
         if isbayesian(model):
-            hyp["pretrain"] = 5
+            hyp["pretrain"] = 5 if not args.test else 0
             module = VI_BNN(args, data, hyp, GPU=args.GPU)
         else:
             if model == "MC_DROPOUT":
@@ -138,10 +144,13 @@ if __name__ == "__main__":
                     f"Wrong model {model}. Available : MFVI, "
                     "RADIAL, LOWRANK, MC-DROPOUT, DEEP-ENSEMBLE, HETERO-NN"
                 )
-
-        module.fit(args.epochs, monitor=monitor, early_stop=args.early_stop)
+        if not args.test:
+            module.fit(args.epochs, monitor=monitor, early_stop=args.early_stop)
         module.test()
-        try:
-            module.epistemic_aleatoric_uncertainty(device=torch.device("cpu"))
-        except Exception:
-            pass
+        if not args.test:
+            try:
+                module.epistemic_aleatoric_uncertainty(
+                    device=torch.device("cpu")
+                )
+            except Exception:
+                pass